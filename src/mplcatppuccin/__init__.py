ee5689358a04ef3ec1b5138601895ba3f117ff4e
miloth <milo.knightword@gmail.com>
2024-06-18 13:25:59 +0100

    feat: Package deprecation (#3)
    * Added deprecation warning at package import
    
    * Added deprecation warning and upgrade instructions to README
    
    * README message higher up
    
    * More
    
    * Deprecation README
    
    * Title edit
    
    * Update src/mplcatppuccin/__init__.py
    
    Co-authored-by: uncenter <47499684+uncenter@users.noreply.github.com>
    
    * Update README.md
    
    Co-authored-by: uncenter <47499684+uncenter@users.noreply.github.com>
    
    * Package to upgrade in warning message
    
    ---------
    
    Co-authored-by: uncenter <47499684+uncenter@users.noreply.github.com>

--- src/mplcatppuccin/__init__.py
+++ src/mplcatppuccin/__init__.py
@@ -1,5 +1,7 @@
 """Soothing pastel theme for matplotlib"""
 
+import warnings
+
 import matplotlib.style
 import matplotlib as mpl
 import pkg_resources
@@ -8,6 +10,12 @@ from .colormaps import get_colormap_from_list
 __version__ = pkg_resources.require("catppuccin-matplotlib")[0].version
 __author__ = "Bram de Wilde <contact@bramdewilde.com>"
 __all__ = []
+
+warning_message = """The catppuccin-matplotlib package is deprecated, please upgrade to https://github.com/catppuccin/python (pip install catppuccin)
+
+"""
+
+warnings.warn(warning_message, DeprecationWarning, stacklevel=2)
 
 # Register the included stylesheet in the mpl style library
 data_path = pkg_resources.resource_filename("mplcatppuccin", "data/")
